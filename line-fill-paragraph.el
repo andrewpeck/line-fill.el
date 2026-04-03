906bb5907cfc5c16af8ef00aed4fdc35ba519ea2
Andrew Peck <peckandrew@gmail.com>
2026-04-03 09:14:59 -0700

    refactor: (if (not x) a b) -> (if x b a)

--- line-fill-paragraph.el
+++ line-fill-paragraph.el
@@ -38,54 +38,53 @@
   "When called with prefix argument P call `fill-paragraph'.
 Otherwise split the current paragraph into one sentence per line."
   (interactive "P")
-  (if (not P)
-      (let ((abbrev-regexp
-             (concat "\\s-+\\("
-                     (string-join (mapcar #'regexp-quote line-fill-paragraph-non-separators) "\\|")
-                     "\\)"))
-            (paren-end-regexp "[!?][\"')]+\\'"))
-        (save-excursion
-          (let ((fill-column 12345678)) ;; relies on dynamic binding
-            (fill-paragraph) ;; this will not work correctly if the paragraph is
-            ;; longer than 12345678 characters (in which case the
-            ;; file must be at least 12MB long. This is unlikely.)
-            (let ((end (save-excursion
-                         (forward-paragraph 1)
-                         (backward-sentence)
-                         (point-marker)))) ;; remember where to stop
-              (beginning-of-line)
-              (catch 'line-fill-paragraph-done
-                (while t
-                  ;; advance one sentence; exit cleanly at end of buffer or past end
-                  (condition-case nil
-                      (forward-sentence)
-                    (end-of-buffer (throw 'line-fill-paragraph-done nil)))
-                  (when (> (point) (marker-position end))
-                    (throw 'line-fill-paragraph-done nil))
-                  ;; skip over abbreviations (e.g., i.e.) and punct inside parens (or more!)
-                  (let ((skipped nil)
-                        (s (point))
-                        (e nil))
-                    (save-excursion
-                      (when (re-search-backward " " nil t)
-                        (setq e (point))))
-                    (when e
-                      (let ((last-word (buffer-substring-no-properties s e)))
-                        (when (or (string-match abbrev-regexp last-word)
-                                  (string-match paren-end-regexp last-word))
-                          (setq skipped t)
-                          (condition-case nil
-                              (forward-sentence)
-                            (end-of-buffer (throw 'line-fill-paragraph-done nil)))
-                          (when (> (point) (marker-position end))
-                            (throw 'line-fill-paragraph-done nil)))))
-                    (unless skipped
-                      (just-one-space) ;; leaves only one space, point is after it
-                      (delete-char -1) ;; delete the space
-                      (newline)        ;; and insert a newline
-                      (indent-region (line-beginning-position) (line-end-position))))))))))
-    ;; otherwise do ordinary fill paragraph
-    (fill-paragraph P)))
+  ;; ordinary fill paragraph when prefix arg is set
+  (if P (fill-paragraph P)
+    (let ((abbrev-regexp
+           (concat "\\s-+\\("
+                   (string-join (mapcar #'regexp-quote line-fill-paragraph-non-separators) "\\|")
+                   "\\)"))
+          (paren-end-regexp "[!?][\"')]+\\'"))
+      (save-excursion
+        (let ((fill-column 12345678)) ;; relies on dynamic binding
+          (fill-paragraph) ;; this will not work correctly if the paragraph is
+          ;; longer than 12345678 characters (in which case the
+          ;; file must be at least 12MB long. This is unlikely.)
+          (let ((end (save-excursion
+                       (forward-paragraph 1)
+                       (backward-sentence)
+                       (point-marker)))) ;; remember where to stop
+            (beginning-of-line)
+            (catch 'line-fill-paragraph-done
+              (while t
+                ;; advance one sentence; exit cleanly at end of buffer or past end
+                (condition-case nil
+                    (forward-sentence)
+                  (end-of-buffer (throw 'line-fill-paragraph-done nil)))
+                (when (> (point) (marker-position end))
+                  (throw 'line-fill-paragraph-done nil))
+                ;; skip over abbreviations (e.g., i.e.) and punct inside parens (or more!)
+                (let ((skipped nil)
+                      (s (point))
+                      (e nil))
+                  (save-excursion
+                    (when (re-search-backward " " nil t)
+                      (setq e (point))))
+                  (when e
+                    (let ((last-word (buffer-substring-no-properties s e)))
+                      (when (or (string-match abbrev-regexp last-word)
+                                (string-match paren-end-regexp last-word))
+                        (setq skipped t)
+                        (condition-case nil
+                            (forward-sentence)
+                          (end-of-buffer (throw 'line-fill-paragraph-done nil)))
+                        (when (> (point) (marker-position end))
+                          (throw 'line-fill-paragraph-done nil)))))
+                  (unless skipped
+                    (just-one-space) ;; leaves only one space, point is after it
+                    (delete-char -1) ;; delete the space
+                    (newline)        ;; and insert a newline
+                    (indent-region (line-beginning-position) (line-end-position))))))))))))
 
 (provide 'line-fill-paragraph)
 ;;; line-fill-paragraph.el ends here
